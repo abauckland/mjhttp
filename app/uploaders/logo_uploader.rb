2dc815fa7da57090586da5b6a66ff6eb4c9e3475
Andrew Burton <abauckland@gmail.com>
2014-11-14 17:32:30 +0000

    edit uploader config route

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -2,9 +2,9 @@ class LogoUploader < CarrierWave::Uploader::Base
 
   storage :file
  
-#  CarrierWave.configure do |config|
-#    config.root = "https://www.myhq.org.uk/shared/public/"
-#  end
+  CarrierWave.configure do |config|
+    config.root = ""
+  end
 
   def store_dir
     File.join("https://www.myhq.org.uk/shared/public/", "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}")
