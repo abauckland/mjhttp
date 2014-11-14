a7574cf4dcfe88b889fdbbcbfe4325f83dd7efed
Andrew Burton <abauckland@gmail.com>
2014-11-14 17:27:42 +0000

    update root with File.join

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -1,17 +1,13 @@
 class LogoUploader < CarrierWave::Uploader::Base
 
   storage :file
-
-
-
-  
-  CarrierWave.configure do |config|
-    config.root = "https://www.myhq.org.uk/shared/public/"
-  end
-
+ 
+#  CarrierWave.configure do |config|
+#    config.root = "https://www.myhq.org.uk/shared/public/"
+#  end
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    File.join("https://www.myhq.org.uk/shared/public/", "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}")
   end
 
 end
