02c5b2ba87e7680afce66e5721cbf64e7e0c457f
Andrew Burton <abauckland@gmail.com>
2014-11-14 16:59:07 +0000

    uploader root ref change

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -2,8 +2,16 @@ class LogoUploader < CarrierWave::Uploader::Base
 
   storage :file
 
+
+
+  
+  CarrierWave.configure do |config|
+    config.root = "https://www.myhq.org.uk/shared/public/"
+  end
+
+
   def store_dir
-    "https://www.myhq.org.uk/shared/public/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
 end
