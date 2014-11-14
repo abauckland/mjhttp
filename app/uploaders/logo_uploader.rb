17413692fb4c3ea3babf3f29b7ba46ee6ad71307
Andrew Burton <abauckland@gmail.com>
2014-11-14 16:42:44 +0000

    update to logo uploader
    change url to get correct image

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -3,7 +3,7 @@ class LogoUploader < CarrierWave::Uploader::Base
   storage :file
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "https://www.myhq.org.uk/shared/public/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
 end
